5e594a05125fdeb0c53353b5c5b6c72d5b04fed7
bony237 <nbj.ngoube@gmail.com>
2020-07-23 17:11:12 +0100

    localized - templates - webpage

--- assets/js/your-js-filename.js
+++ assets/js/your-js-filename.js
@@ -1,1 +1,10 @@
+/* Menu-sm event*/
+var checkMenu = document.querySelector("#btn-menu-sm");
+      checkMenu.addEventListener('change', function() {
+        document.querySelector('#menu-sm').classList.toggle('d-none');
+      });
 
+      
+
+/*AOS library initialisation*/
+AOS.init();
